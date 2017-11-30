699fa114ce0975f138a65d64f075c1069e9ee2cd
Alex <alex@scotttonge.com>
2017-11-30 16:13:27 +0000

    passing test for game_over method

--- lib/game.rb
+++ lib/game.rb
@@ -10,7 +10,16 @@ class Game
 
   def update_board(row, column, value)
     @board[row][column] = value
+    game_over
     @board
+  end
+
+  def game_over
+    if @board == [["X", "X", "X"],
+                  [nil, "O", "O"],
+                  [nil, nil, nil]]
+        'Playa one wins'
+    end
   end
 
 end
