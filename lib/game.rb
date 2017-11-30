796b9a86e03e106483a9a65793ba2e7352c0f155
Alex <alex@scotttonge.com>
2017-11-30 16:06:49 +0000

    failing test for game_over method

--- lib/game.rb
+++ lib/game.rb
@@ -3,13 +3,14 @@ class Game
   attr_reader :board
 
   def initialize
-    @board = [[nil,nil,nil],
-              [nil,nil,nil],
-              [nil,nil,nil]]
+    @board = [[nil, nil, nil],
+              [nil, nil, nil],
+              [nil, nil, nil]]
   end
 
   def update_board(row, column, value)
     @board[row][column] = value
+    @board
   end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -5,18 +5,30 @@ describe Game do
   let(:game) { described_class.new }
 
   it 'displays an empty board' do
-    expect(game.board).to eq [[nil,nil,nil],
-                              [nil,nil,nil],
-                              [nil,nil,nil]]
+    expect(game.board).to eq [[nil, nil, nil],
+                              [nil, nil, nil],
+                              [nil, nil, nil]]
   end
 
   describe '#update_board' do
     it 'updates a field with a value' do
-      game.update_board(1,1,'X')
-      expect(game.board).to eq [[nil,nil,nil],
-                                [nil,'X',nil],
-                                [nil,nil,nil]]
+      game.update_board(1, 1, 'X')
+      expect(game.board).to eq [[nil, nil, nil],
+                                [nil, 'X', nil],
+                                [nil, nil, nil]]
     end
   end
+
+  describe '#game_over' do
+    it 'finishes the game after 3 in a row' do
+      game.update_board(0, 0, 'X')
+      game.update_board(1, 1, 'O')
+      game.update_board(0, 1, 'X')
+      game.update_board(1, 2, 'O')
+      game.update_board(0, 2, 'X')
+      expect(game.game_over).to eq 'Playa one wins'
+    end
+  end
+
 
 end
